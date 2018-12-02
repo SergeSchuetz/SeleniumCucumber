301890dffeec38b7bc08c2f4c2b1df8c1b74393b
SergeSchuetz <39755568+SergeSchuetz@users.noreply.github.com>
2018-12-02 19:04:50 +1100

    Add files via upload

--- src/test/java/StepDefinitions/MyStepdefsTest.java
+++ src/test/java/StepDefinitions/MyStepdefsTest.java
@@ -3,15 +3,12 @@ package StepDefinitions;
 import cucumber.api.java.en.Given;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
 
 public class MyStepdefsTest {
 
     private static WebDriver driver;
 
     @Given("^I open \"([^\"]*)\" page$")
-    @Test
     public void iOpenPage(String url) {
         // Write code here that turns the phrase above into concrete actions
         System.setProperty("WebDriver.chrome.driver", "src/test/resources/drivers/chromedriver");
